1169ad8249a9e8ab1d4538408659e87c563e3783
Keegan <b.keegan12@yahoo.com>
2020-08-05 11:55:21 -0400

    First semi-working build

--- Request.cs
+++ Request.cs
@@ -10,12 +10,15 @@ namespace Super_Simple_Webserver
         public String Type { get; set; }
         public String URL { get; set; }
         public String Host { get; set; }
+        public String Referer { get; set; }
 
-        private Request(String type, String url, String host)
+
+        private Request(String type, String url, String host, String referer)
         {
             Type = type;
             URL = url;
             Host = host;
+            Referer = referer;
         }
 
         public static Request GetRequest(String request)
@@ -28,8 +31,18 @@ namespace Super_Simple_Webserver
             String type = tokens[0];
             String url = tokens[1];
             String host = tokens[4];
+            String referer = "";
 
-            return new Request(type, url, host);
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == "Referer:")
+                {
+                    referer = tokens[i + 1];
+                    break;
+                }
+            }
+
+            return new Request(type, url, host, referer);
         }
     }
 }
--- Response.cs
+++ Response.cs
@@ -29,12 +29,50 @@ namespace Super_Simple_Webserver
             
             if (request.Type == "GET")
             {
+                String file = Environment.CurrentDirectory + HTTPServer.WEB_DIR + request.URL;
+                FileInfo f = new FileInfo(file);
+                if (f.Exists && f.Extension.Contains("."))
+                {
+                    return MakeFromFile(f);
+                }
+                else
+                {
+                    DirectoryInfo info = new DirectoryInfo(f + "/");
+                    if (!info.Exists)
+                    {
+                        return MakePageNotFound();
+                    }
+                    FileInfo[] files = info.GetFiles();
+                    foreach (FileInfo ff in files)
+                    {
+                        String name = ff.Name.ToLower();
+                        if (name.Contains("default.html") || name.Contains("default.htm") || name.Contains("index.html") || name.Contains("index.htm")) 
+                        {
+                            f = ff;
+                            return MakeFromFile(ff);
+                        }
+                    }
+                }
 
+            
             } 
             else
             {
-
+                return MakeMethodNotAllowed();
             }
+            return MakePageNotFound();
+        }
+
+        private static Response MakeFromFile(FileInfo f)
+        {
+        
+            FileStream fileStream = f.OpenRead();
+            BinaryReader reader = new BinaryReader(fileStream);
+            Byte[] d = new byte[fileStream.Length];
+            reader.Read(d, 0, d.Length);
+            fileStream.Close();
+
+            return new Response("200 OK", "html/text", d);
         }
 
         private static Response MakeNullRequest()
@@ -45,8 +83,24 @@ namespace Super_Simple_Webserver
             BinaryReader reader = new BinaryReader(fileStream);
             Byte[] d = new byte[fileStream.Length];
             reader.Read(d, 0, d.Length);
+            fileStream.Close();
+
 
             return new Response("400 Bad Request", "html/text", d);
+        }
+
+        private static Response MakeMethodNotAllowed()
+        {
+            String file = Environment.CurrentDirectory + HTTPServer.MSG_DIR + "405.html";
+            FileInfo fileInfo = new FileInfo(file);
+            FileStream fileStream = fileInfo.OpenRead();
+            BinaryReader reader = new BinaryReader(fileStream);
+            Byte[] d = new byte[fileStream.Length];
+            reader.Read(d, 0, d.Length);
+            fileStream.Close();
+
+
+            return new Response("405 Method Not Allowed", "html/text", d);
         }
 
         private static Response MakePageNotFound()
